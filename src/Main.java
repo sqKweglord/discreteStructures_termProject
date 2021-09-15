e46f8652457411a6e3923d03d9474179af68aa05
sqKweglord <70150559+sqKweglord@users.noreply.github.com>
2021-09-15 15:19:18 -0400

    added method header for sorts and main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+
+    public static void bubble() {
+
+    }
+
+    public static void selection() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
 }
