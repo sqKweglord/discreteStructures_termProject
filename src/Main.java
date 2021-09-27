1394bb8a5d51545d6dd4932db42c7bbdecb1ee98
sqKweglord <70150559+sqKweglord@users.noreply.github.com>
2021-09-27 18:10:51 -0400

    cloned arrays for selection sort

--- src/Main.java
+++ src/Main.java
@@ -28,36 +28,74 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<int[]> arrays1 = returnArrays(500);
-        ArrayList<int[]> arrays2 = returnArrays(2500);
-        ArrayList<int[]> arrays3 = returnArrays(5000);
+        //ArrayList<int[]> arrays1 = returnArrays(500);
+        //ArrayList<int[]> arrays2 = returnArrays(2500);
+        ArrayList<int[]> arrays3 = returnArrays(10000);
 
         ArrayList<ArrayList<int[]>> allArrays = new ArrayList<>(3);
-        allArrays.add(arrays1);
-        allArrays.add(arrays2);
+        //allArrays.add(arrays1);
+        //allArrays.add(arrays2);
         allArrays.add(arrays3);
 
+        ArrayList<ArrayList<int[]>> allArraysAgain = new ArrayList<>(3);
 
+        for (ArrayList<int[]> z : allArrays) {
+            ArrayList<int[]> newList = new ArrayList<>(1000);
+            for (int[] y : z) {
+                int[] newArray = new int[y.length];
+                for (int i = 0; i < y.length; i++) {
+                    newArray[i] = y[i];
+                }
+                newList.add(newArray);
+            }
+            allArraysAgain.add(newList);
+        }
 
-        ArrayList<Object> cloned = new ArrayList<>(allArrays);
+        /*
+        for (int x = 0; x < 10; x++) {
+            System.out.print(allArrays.get(2).get(980)[x] + "\s");
+        }
+        System.out.println();
+
+        for (int x = 0; x < 10; x++) {
+            System.out.print(allArraysAgain.get(2).get(980)[x] + "\s");
+        }
+        System.out.println();
+        System.out.println();
+        */
 
 
         try {
-
-
+            /*
             //intializes the sorts
             Sort[] sorts = new Sort[6];
             for (int i = 1; i < 7; i++) {
                 if (i < 4) {
                     sorts[i-1] = new Sort(allArrays.get(i-1), 1);
                 } else {
-                    sorts[i-1] = new Sort((ArrayList<int[]>) cloned.get(i-4), 2);
+                    sorts[i-1] = new Sort((ArrayList<int[]>) allArraysAgain.get(i-4), 2);
                 }
 
             }
 
             Thread[] threads = new Thread[6];
             for (int i = 0; i < 6; i++) {
+                threads[i] = new Thread(sorts[i]);
+            }
+            */
+
+            Sort[] sorts = new Sort[2];
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) {
+                    sorts[i-1] = new Sort(allArrays.get(0), 1);
+                } else {
+                    sorts[i-1] = new Sort((ArrayList<int[]>) allArraysAgain.get(0), 2);
+                }
+
+            }
+
+            Thread[] threads = new Thread[2];
+            for (int i = 0; i < 2; i++) {
                 threads[i] = new Thread(sorts[i]);
             }
 
